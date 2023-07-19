c939db4ec6e71f2ffaaf32699a521e675c122f8c
vlj2007 <vlj2007@yandex.ru>
2023-07-19 18:50:21 +0700

    I_have_finished_task2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
-    public static void task1(){
+    public static void task1() {
         System.out.println("Задача 1");
         int clientOS = 0;
 
-        if (clientOS == 0){
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+
+        if (clientOS == 0) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+        if (clientOS == 1) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
     }
 }
