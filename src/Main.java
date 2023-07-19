3b17a6bbd46314ad75baf29cafce1b547ab0ba7e
vlj2007 <vlj2007@yandex.ru>
2023-07-19 17:38:55 +0700

    I_have_finished_task1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
     public static void task1(){
         System.out.println("Задача 1");
+        int clientOS = 0;
 
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
